a75b51266c901f779e26a0bfd262a13ca108c94c
Tarun Chawla <65712331+Tarun193@users.noreply.github.com>
2023-07-22 20:58:21 -0400

    added site map functionallity

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [
+    tailwind(),
+    sitemap({
+      customPages: ["https://tarun-chawla.tech"],
+    }),
+  ],
   site: "https://www.blog.tarun-chawla.tech",
 });
