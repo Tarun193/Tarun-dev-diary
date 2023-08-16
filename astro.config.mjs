9bf95538344a45b7a8a27dca76da932a0a503251
Tarun Chawla <65712331+Tarun193@users.noreply.github.com>
2023-08-16 18:38:43 -0400

    added page transitions

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,15 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap({
-    customPages: ["https://tarun-chawla.tech"]
-  }), react()],
-  site: "https://www.blog.tarun-chawla.tech"
+  integrations: [
+    tailwind(),
+    sitemap({
+      customPages: ["https://tarun-chawla.tech"],
+    }),
+    react(),
+  ],
+  experimental: {
+    viewTransitions: true,
+  },
+  site: "https://www.blog.tarun-chawla.tech",
 });
